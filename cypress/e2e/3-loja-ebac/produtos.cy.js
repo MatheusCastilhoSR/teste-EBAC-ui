101f56d530c0f5c040c62e7ce2eb5c1351cbfca6
Matheus Castilho <matheuscs.ribeiro15@gmail.com>
2025-03-26 16:50:38 -0300

    Testes com lista

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,18 @@
+/// <Reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()  PRIMEIRO ITEM DA LISTA
+            //.last()   ÚLTIMO ITEM DA LISTA
+            //.eq(2)    SELEÇÃO POR ESCOLHA DE POSIÇÃO NA LISTA
+            .contains('Aero Daily Fitness Tee')
+            .click()
+
+            cy.get('#tab-description > :nth-child(1)').should('contain' , 'Need an everyday action tee that helps keep you dry? The Aero Daily Fitness Tee is made of 100% polyester wicking knit that funnels moisture away from your skin. Don’t be fooled by its classic style; this tee hides premium performance technology beneath its unassuming look.')
+    });
+});
